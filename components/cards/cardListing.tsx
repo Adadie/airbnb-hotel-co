cea878f7de470ee4d75383b394c7992660b8e790
Adadie_K <junioradadie@gmail.com>
2023-10-26 04:32:21 +0200

    ch: add more dummy data

--- components/cards/cardListing.tsx
+++ components/cards/cardListing.tsx
@@ -3,13 +3,117 @@ import ListingCard from './listingCard';
 
 interface CardListingProps {}
 
+const listingItems = [
+  {
+    id: 1,
+    image: '/images/listing/image7.webp',
+    name: 'Balina Beach, Indonesia',
+    distance: '12,900',
+    price: '$1,098',
+    liked: true,
+  },
+  {
+    id: 2,
+    image: '/images/listing/image1.webp',
+    name: 'Sisily',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 3,
+    image: '/images/listing/image2.webp',
+    name: 'Turkey',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 4,
+    image: '/images/listing/image8.webp',
+    name: 'Abiansemal, Indonesia',
+    distance: '12,900',
+    price: '$990',
+    liked: true,
+  },
+  {
+    id: 5,
+    image: '/images/listing/image2.webp',
+    name: 'Turkey',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 6,
+    image: '/images/listing/image1.webp',
+    name: 'Sisily',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 7,
+    image: '/images/listing/image7.webp',
+    name: 'Balina Beach, Indonesia',
+    distance: '12,900',
+    price: '$1,098',
+    liked: true,
+  },
+  {
+    id: 8,
+    image: '/images/listing/image1.webp',
+    name: 'Sisily',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 9,
+    image: '/images/listing/image8.webp',
+    name: 'Abiansemal, Indonesia',
+    distance: '12,900',
+    price: '$990',
+    liked: true,
+  },
+  {
+    id: 10,
+    image: '/images/listing/image7.webp',
+    name: 'Balina Beach, Indonesia',
+    distance: '12,900',
+    price: '$1,098',
+    liked: true,
+  },
+  {
+    id: 11,
+    image: '/images/listing/image1.webp',
+    name: 'Sisily',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+  {
+    id: 12,
+    image: '/images/listing/image2.webp',
+    name: 'Turkey',
+    distance: '12,900',
+    price: '$990',
+    liked: false,
+  },
+];
 const CardListing: React.FC<CardListingProps> = () => {
   return (
     <div className='w-full p-4'>
-    <div className='grid grid-row grid-cols-4 gap-10 gap-y-96'>
-        {new Array(20).fill(undefined).map((_, index) => (
-            <div key={index}>
-          <ListingCard />
+      <div className='grid grid-row grid-cols-4 gap-10 gap-y-96'>
+        {listingItems.map((item, index) => (
+          <div key={index}>
+            <ListingCard
+              liked={item.liked}
+              name={item.name}
+              image={item.image}
+              distance={item.distance}
+              price={item.price}
+            />
           </div>
         ))}
       </div>
--- components/cards/listingCard.tsx
+++ components/cards/listingCard.tsx
@@ -2,33 +2,45 @@ import Link from 'next/link';
 import React from 'react';
 import { FaHeart } from 'react-icons/fa'; // Import the heart icon
 
-interface ListingCardProps {}
+interface ListingCardProps {
+  name: string;
+  image: string;
+  price: string;
+  distance: string;
+  liked: boolean;
+}
 
-const ListingCard: React.FC<ListingCardProps> = () => {
+const ListingCard: React.FC<ListingCardProps> = ({
+  name,
+  image,
+  price,
+  distance,
+  liked,
+}) => {
   return (
     <>
       <Link href='/listings/1'>
         <div className='max-w-sm h-20 cursor-pointer bg-white border border-gray-200 rounded-lg shadow relative'>
           <>
-            <img
-              className='rounded-t-lg'
-              src='/images/listing/image6.webp'
-              alt=''
-            />
-            <FaHeart className='absolute top-2 right-2 text-red-500 cursor-pointer' />
+            <img className='rounded-lg h-52 w-80 object-cover' src={image} alt='' />
+            {liked ? (
+              <FaHeart className='absolute top-2 right-2 text-red-500 cursor-pointer' />
+            ) : (
+              <FaHeart className='absolute top-2 right-2 text-gray-800 opacity-70 cursor-pointer' />
+            )}
           </>
-          <div className='p-5 space-y-2'>
+          <div className='py-5 space-y-2'>
             <div className='text-md font-bold tracking-tight text-gray-900 '>
-              BALIAN TREEHOUSE w beautiful pool
+              {name}
             </div>
             <div className='font-medium text-gray-600'>
-              7,000 kilometers away
+              {distance} kilometers away
             </div>
             <div className='font-medium text-gray-600'>
               5 nights . Nov 18 - 23
             </div>
             <div className='text-md  font-medium underline text-gray-950'>
-              $1,078 total before taxes
+              {price} total before taxes
             </div>
           </div>
         </div>
